4694867ce0d30adaaf44f21735adade86e944f3c
SUN-0207 <cuthanhson27@gmail.com>
2026-06-11 00:14:55 +0700

    feat(api): register job-failure listener + retry reconciler in JobsModule

--- apps/api/src/modules/jobs/jobs.module.ts
+++ apps/api/src/modules/jobs/jobs.module.ts
@@ -1,11 +1,13 @@
 import { QueueModule } from '@/modules/queue/queue.module';
 import { Module } from '@nestjs/common';
+import { JobFailureListener } from './job-failure.listener';
 import { JobsController } from './jobs.controller';
 import { JobsService } from './jobs.service';
+import { RetryReconcilerService } from './retry-reconciler.service';
 
 @Module({
   imports: [QueueModule],
   controllers: [JobsController],
-  providers: [JobsService],
+  providers: [JobsService, JobFailureListener, RetryReconcilerService],
 })
 export class JobsModule {}
